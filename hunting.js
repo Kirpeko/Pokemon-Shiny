c38f4bb1e82acd2e6d32a7f4f8f9947c12825ddd
Nick <Nick@NICKMANPC>
2021-03-02 10:54:39 -0330

    Formatting changes

--- hunting.js
+++ hunting.js
@@ -19,12 +19,15 @@ function main(){
     let new_button = document.createElement("button");
     let pokemon_list = document.querySelector("#hunting_list");
 
-
+    
 
     function onConfirmButtonClick(){
+        selected_mon = document.querySelector("#pokemon_dropdown").value;
         new_button.id = "testid";
         pokemon_list.appendChild(new_button);
-        new_button.style.cssText = "height: 100px;padding:10px;"
+        new_button.style.cssText = "display: flex;flex-direction: column;height: 100px;padding: 10px;justify-content: center;align-items: center;font-size: 25px;";
+        new_button.innerHTML = `${selected_mon}  ·  ${0}`;
+            // Read Chosen pokemon name/set counter to 0
     }
 
     confirm_button.addEventListener("click", onConfirmButtonClick);
